1a7f40ad38f7a233287381a9baea73375b81ba2e
turkpenbayev <mr.turkpenbaev1997@gmail.com>
2023-03-31 16:58:20 +0500

    added threads

--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
 
-    SHUNK_SIZE = 1024 * 1024
+    MAX_WORKERS = 4
+    CHUNK_SIZE = 1024 * 1024
     AWS_ACCESS_KEY_ID: str = 'AKIAXTD25Q6VOFW55JJZ'
     AWS_SECRET_KEY: str = '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'
     AWS_S3_BUCKET_NAME: str = 'bauka'
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from io import BytesIO
 
 import boto3
 from botocore.exceptions import ClientError
+from concurrent.futures import ThreadPoolExecutor
 from fastapi import FastAPI, File, UploadFile
 from app.config import settings
 
@@ -17,18 +18,22 @@ s3 = boto3.client(
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    chunk_count = 0
+    futures = []
     try:
-        while True:
-            chunk = file.file.read(settings.SHUNK_SIZE)
-            if not chunk:
-                break
-            chunk_count += 1
-            s3.upload_fileobj(
-                BytesIO(chunk),
-                settings.AWS_S3_BUCKET_NAME,
-                f"{file.filename}.part{chunk_count}"
-            )
+        with ThreadPoolExecutor(max_workers=settings.MAX_WORKERS) as executor:
+            while True:
+                chunk = file.file.read(settings.CHUNK_SIZE)
+                if not chunk:
+                    break
+                future = executor.submit(upload_chunk_to_s3, chunk, file.filename, len(futures) + 1)
+                futures.append(future)
+        for future in futures:
+            future.result()
     except ClientError as e:
         return {"success": False}
     return {"success": True}
+
+
+def upload_chunk_to_s3(chunk, filename, chunk_number):
+    key = f"{filename}.part{chunk_number}"
+    s3.upload_fileobj(BytesIO(chunk), settings.AWS_S3_BUCKET_NAME, key)
